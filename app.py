9bd06ac0dffe433f745f16376016ab92fc6e97a1
B phan <bphan@Bs-MacBook-Air.local>
2022-04-13 17:28:20 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "hellomyfirstapp"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db' 
+
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+import routes, models
